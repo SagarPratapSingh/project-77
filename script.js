28b45c5d6da86fccc0983870e7d51199d8ee479c
SagarPratapSingh <81958432+SagarPratapSingh@users.noreply.github.com>
2021-08-11 09:56:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("My first console project")
+var first_name = "Sagar";
+var last_name = "Singh";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a = 16;
+var b = 9;
+var add = a + b;
+console.log(add);
+var c = 25;
+var d = 15;
+var sub = c - d;
+console.log(sub);
+var e = 12;
+var f = 8;
+var multiply = e * f;
+console.log(multiply);
+var g = 100;
+var h = 5;
+var divide = 100 / 5;
+console.log(divide);
